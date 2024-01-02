e03ade43211cfef0316aa845aa7a28d929c62dca
kimba <skyqwn@gmail.com>
2024-01-02 22:06:38 +0900

    a

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React11
+          Learn React1122
         </a>
       </header>
     </div>
